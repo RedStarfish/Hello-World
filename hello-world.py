888ca035a7835c5744a06e11b268dcf7f1738be5
Red Starfish <contactmefriends@tutanota.com>
2020-02-04 14:28:22 +0530

    Also added age asking question

--- hello-world.py
+++ hello-world.py
@@ -1,2 +1,11 @@
+# This is a simple hello world program
+
+import pyinputplus as pyip
+
 print('Hello, World!')
+print('What is your name?')
+name = pyip.inputStr()
+print(f'Your name is {len(name) + 1} chracter long')
+age = pyip.inputInt('What is your age?')
+print(f'You will be {age + 1} next year')
 
